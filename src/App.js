1a22c3611c4bcb5b9fb5b71a39499c7ea2474066
Francisco Viana <franciscoviana862@gmail.com>
2019-09-15 18:13:06 -0300

    acesso api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import Main from './pages/main/';
+
 import './styles.css';
 
+
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Header / >
-        <
-        /div>
+    return (
+        <div className="App" >
+            <Header />
+            <Main />
+        </div>
     );
 }
 
